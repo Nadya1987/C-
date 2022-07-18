8dc6f80c5bfd7ac86007dff6333c750a5e6cdf24
Nadya1987 <107540138+Nadya1987@users.noreply.github.com>
2022-07-18 21:48:09 +0300

    Семинар 8

--- Seminar1_Task1/Program.cs
+++ Seminar1_Task1/Program.cs
@@ -866,3 +866,198 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2dArray(m,n,min,max);
 Show2dArray(myArray);
 Console.WriteLine("Sum of main diagonal is " + FindDiagSum(myArray));*/
+
+
+//1 Задайте двумерный массив. Напишите программу, которая поменяет 
+//местами первую и последнюю строку массива.
+/*
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 1; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+
+}
+int[,] ChangeArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        int temp = array[0, j];
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+    return array;
+}
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maxpossible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+
+Show2dArray(myArray);
+Show2dArray(ChangeArray(myArray));*/
+
+//2 Задайте двумерный массив. Напишите программу, которая заменяет 
+//строки на столбцы. В случае, если это невозможно, программа должна 
+//вывести сообщение для пользователя.
+//создать матрицу
+/*
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) //строка и аргумент
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+//  получить результат в виде матрицы
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+// метод, который будет переворачивать матрицу
+
+int[,] Reverse2dArray(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Number of rows and columns is not the same");
+        return array;
+    }
+
+    for (int i = 0; i < array.GetLength(0) - 1; i++)
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+
+    return array;
+}
+
+//2 5 1
+//3 7 4
+//9 6 8
+
+//2 3 9
+//5 7 6
+//1 4 8
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+
+Show2dArray(myArray);
+Show2dArray(Reverse2dArray(myArray));*/
+
+//3 Из двумерного массива целых чисел удалить строку и 
+//столбец, а пересечении которых расположен наименьший 
+//элемент
+
+//создать матрицу
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) //строка и аргумент
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+//  получить результат в виде матрицы
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+
+int[,] Cut2dArray(int[,] array)
+{
+    int iMin = 0, jMin = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < array[iMin, jMin])
+            {
+                iMin = i;
+                jMin = j;
+            }
+        }
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[iMin, j] = 0;
+
+    for (int i = 0; iMin < array.GetLength(0); i++)
+        array[iMin, jMin] = 0;
+
+    return array;
+}
+
+//7 4 2 3
+//5 3 6 2
+//6 7 1 3
+//3 2 5 4
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+
+/*Show2dArray(myArray);*/
+Show2dArray(Cut2dArray(myArray));
+
